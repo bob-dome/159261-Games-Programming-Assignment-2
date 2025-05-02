5abd8d45ca731c6789e10c723d33668c34b4dfae
Jordan8899 <burmei.jorda@awatapu.school.nz>
2025-05-02 15:11:36 +1200

    I spent way too long trying to figure out how to make buttons and I have now given up. Basic menu there now I'll move onto the actual gameplay

--- PlatformPanic.java
+++ PlatformPanic.java
@@ -1,5 +1,7 @@
 // 24006168 Jordan Burmeister, 24003491 Wiremu Loader, 24002464 Aimee Gaskin Fryer
 
+import java.awt.Color;
+
 public class PlatformPanic extends GameEngine
 {
     // Variable Creation
@@ -10,13 +12,18 @@ public class PlatformPanic extends GameEngine
     private static int gridSize;
 
     // Main Menu & Pause
-    private static boolean gameMenu;
     private static boolean gamePaused;
+    private static boolean menu;
 
     // Score & Highscore
     private static int score;
     private static int highscore;
 
+    // Single Player Variables
+    private static boolean singlePlayerStarted;
+
+    // Multiplayer Variables
+    private static boolean multiplayerStarted;
 
     // Initialize Variables runs only when the program has been run
     @Override
@@ -27,14 +34,18 @@ public class PlatformPanic extends GameEngine
         gameWidth = 500;
         gridSize = 10;
         setWindowSize(gameWidth, gameHeight);
-
-        // Game Menu & Pause
-        gamePaused = false;
-        gameMenu = true;
-
+        
         // Score & Highscore
         score = 0;
         highscore = loadHighscore();
+        
+        // Set Gamemodes to false so that the user can press a button for which game they want
+        singlePlayerStarted = false;
+        multiplayerStarted = false;
+        
+        // Game Menu & Pause
+        gamePaused = false;
+        menu = true;
     }
 
     // Used to update game logic such as movement
@@ -48,14 +59,39 @@ public class PlatformPanic extends GameEngine
     @Override
     public void paintComponent()
     {
-        // Create Main Menu
-        if (gameMenu)
+        // Create Single Player background etc
+        if (singlePlayerStarted)
         {
-            // Change the Background Colour and Clear it
-            changeBackgroundColor(white);
+            // Change Background to single player background and clear
+            changeBackgroundColor(blue);
             clearBackground(gameWidth, gameHeight);
+        }
 
-            // Menu Buttons
+        // Create Multiplayer background etc
+        else if (multiplayerStarted)
+        {
+            // Change Background to multiplayer background and clear
+            changeBackgroundColor(Color.cyan);
+            clearBackground(gameWidth, gameHeight);
+        }
+
+        else if (menu)
+        {
+            // Change Background to single player background and clear
+            changeBackgroundColor(Color.black);
+            clearBackground(gameWidth, gameHeight);
+            
+            // Change Text Colour
+            changeColor(white);
+
+            // Draw Title (can be changed to img logo if added later)
+            drawText(height() / 2 - 250, width() / 2 - 100, "Platform Panic", "Arial", 40);
+
+            // Draw Single Player Label, this can be changed later on to a button or we can check if the user presses on this area
+            drawText(height() / 2 - 250, width() / 2, "     Single Player", "Arial", 30);
+
+            // Draw Multiplayer Label, this can be changed later on to a button or we can check if the user presses on this area
+            drawText(height() / 2 - 250, width() / 2 + 50, "       Multiplayer", "Arial", 30);
         }
     }
 
@@ -68,5 +104,27 @@ public class PlatformPanic extends GameEngine
     {
         PlatformPanic platformPanic = new PlatformPanic();
         GameEngine.createGame(platformPanic, 10);
+    }
+
+    // Start Single Player Gamemode
+    public void startSinglePlayer()
+    {
+        // Set Variables
+        menu = false;
+        singlePlayerStarted = true;
+
+        // Debug
+        System.out.println("Single Player Started");
+    }
+
+    // Start Multiplayer Gamemode
+    public void startMultiplayer()
+    {
+        // Set Variables for Multiplayer
+        menu = false;
+        multiplayerStarted = true;
+
+        // Debug
+        System.out.println("Multiplayer Started");
     }
 }
